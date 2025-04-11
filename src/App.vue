b8e1f7141b037e11c3ecb3a126d53583a8ce4b74
paul kuriakose <polkboban@gmail.com>
2025-04-12 00:23:33 +0530

    fifthcomm

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@ import Navbar from './components/Navbar.vue'
     <main class="py-0">
       <router-view></router-view>
     </main>\
-    <footer class="bg-white dark:bg-slate-900 shadow-sm border-t border-slate-100 dark:border-slate-800">
-      <div class="container mx-auto px-4 py-2">
+    <footer class="bg-white dark:bg-slate-900 shadow-sm  dark:border-slate-800">
+      <div class="container mx-auto px-4 py-1 flex flex-col items-center justify-center">
         <p class="text-center text-slate-600 dark:text-slate-300">
           &copy;2025 AlgoViz. Fucking nothins reserved.
         </p>
