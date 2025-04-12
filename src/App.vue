823ef760011a5ef079285f11ed570b03e044f03d
paul kuriakose <polkboban@gmail.com>
2025-04-12 14:41:16 +0530

    sixcomm

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import Navbar from './components/Navbar.vue'
     <footer class="bg-white dark:bg-slate-900 shadow-sm  dark:border-slate-800">
       <div class="container mx-auto px-4 py-1 flex flex-col items-center justify-center">
         <p class="text-center text-slate-600 dark:text-slate-300">
-          &copy;2025 AlgoViz. Fucking nothins reserved.
+          &copy;2025 AlgoViz. Fuckin nothins reserved.
         </p>
       </div>
     </footer>
